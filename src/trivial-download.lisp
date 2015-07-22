5e3056146d8359b0eef0afd72b2ad437585517d1
Matthew Stickney <mstickney@compassmax.com>
2015-07-21 20:23:43 -0400

    Handle 0-size files correctly.

--- src/trivial-download.lisp
+++ src/trivial-download.lisp
@@ -45,11 +45,15 @@
 (defun human-file-size (size)
   "Take a file size (in bytes), return it as a human-readable string."
   (let ((pair (loop for pair in +size-symbol-map+
-                    if (>= size (car pair)) return pair)))
+                    if (or (>= size (car pair))
+                           (= (car pair) 1))
+                    return pair)))
     (format nil "~f ~A" (/ size (car pair)) (cdr pair))))
 
 (defun percentage (total-bytes current-bytes)
-  (floor (/ (* current-bytes 100) total-bytes)))
+  (if (= current-bytes 0)
+      100
+      (floor (/ (* current-bytes 100) total-bytes))))
 
 (defmacro with-download (url (file-size total-bytes-read array stream)
                          &body body)
